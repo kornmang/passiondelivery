a3801b5796b16f541670cc7f69fbfa0538711e22
KO2N <95019375+kornmang@users.noreply.github.com>
2023-01-09 10:14:05 +0700

    folk

---
+++ public/js/tdee.js
@@ -0,0 +1,205 @@
+//set width of results and info divs to the same as the input form width
+window.onload = () => {
+    document.querySelector("#resultsContainer").style.width = window.getComputedStyle(document.querySelector("#inputsContainer")).getPropertyValue("width");
+    document.querySelector("#infoContainer").style.width = window.getComputedStyle(document.querySelector("#inputsContainer")).getPropertyValue("width");
+};
+
+const KILOGRAMS_PER_POUND = 0.4536;
+const CENTIMETERS_PER_INCH = 2.54;
+const CM2_PER_M2 = 10000;
+
+const MIN_CAL_FEMALE = 1200;
+const MIN_CAL_MALE = 1500;
+
+const MALE_CAL_MODIFIER = 5;
+const FEMALE_CAL_MODIFIER = -161;
+
+
+function validateFormInputs(inputs) {
+    inputs.age = parseInt(document.querySelector("#age").value);
+    inputs.weight = parseInt(document.querySelector("#weight").value);
+    inputs.height = parseInt(document.querySelector("#height").value);
+
+    inputs.bodyFatPercent = parseInt(document.querySelector("#bodyFatPercent").value);
+    inputs.bodyFatEntered = false;
+
+    if (!isNaN(inputs.bodyFatPercent)) {
+        inputs.bodyFatEntered = true;
+
+        if (isNaN(inputs.bodyFatPercent) || inputs.bodyFatPercent < 0 || inputs.bodyFatPercent > 100) {
+            alert("Please enter a valid body fat percentage!");
+            return false;
+        }
+    }
+
+    if (isNaN(inputs.age) || inputs.age === "" || inputs.age < 0) {
+        alert("Please enter a valid age!");
+        return false;
+    }
+
+    if (isNaN(inputs.weight) || inputs.weight === "" || inputs.weight < 0) {
+        alert("Please enter a valid weight!");
+        return false;
+    }
+
+    if (isNaN(inputs.height) || inputs.height === "" || inputs.height < 0) {
+        alert("Please enter a valid height!");
+        return false;
+    }
+
+
+    // getting values of dropdowns
+    const gender = document.querySelector("#gender");
+    const weightUnit = document.querySelector("#weightUnit");
+    const heightUnit = document.querySelector("#heightUnit");
+    const activityLevel = document.querySelector("#activityLevel");
+
+    inputs.gender = gender.options[gender.selectedIndex].value;
+    inputs.weightUnit = weightUnit.options[weightUnit.selectedIndex].value;
+    inputs.heightUnit = heightUnit.options[heightUnit.selectedIndex].value;
+    inputs.activityLevel = activityLevel.options[activityLevel.selectedIndex].value;
+
+    return true;
+}
+
+
+
+function calculateTDEEnoBF(gender, age, weight, weightUnit, height, heightUnit, activityMultiplier) {
+    // Mifflin St. Jeor
+    // Mifflin = (10.m + 6.25h - 5.0a) + s
+    // m is mass in kg, h is height in cm, a is age in years, s is +5 for males and -151 for females
+
+    const safeMinCalories = (gender === "M") ? MIN_CAL_MALE : MIN_CAL_FEMALE;
+    const genderModifier = (gender === "M") ? MALE_CAL_MODIFIER : FEMALE_CAL_MODIFIER;
+
+    if (weightUnit === "LBS") {
+        weight *= KILOGRAMS_PER_POUND;
+    }
+
+    if (heightUnit === "IN") {
+        height *= CENTIMETERS_PER_INCH;
+    }
+
+    const BMR = (10 * weight) + (6.25 * height) - (5.0 * age) + genderModifier;
+
+    // if tdee is under safe min calories, then set tdee to safe min calories
+    const TDEE = Math.max(safeMinCalories, Math.round(BMR * activityMultiplier));
+
+    return TDEE;
+}
+
+
+
+function calculateTDEEwithBF(gender, weight, weightUnit, bodyFatPercent, activityMultiplier) {
+    // Katch-McArdle
+    // Katch = 370 + (21.6 * LBM)
+    // where LBM is lean body mass
+
+    const safeMinCalories = (gender === "M") ? MIN_CAL_MALE : MIN_CAL_FEMALE;
+
+    if (weightUnit === "LBS") {
+        weight *= KILOGRAMS_PER_POUND;
+    }
+
+    const LBM = (100 - bodyFatPercent) * 0.01 * weight;
+    const BMR = (21.6 * LBM) + 370;
+
+    const TDEE = Math.round(BMR * activityMultiplier);
+
+    return TDEE;
+}
+
+
+
+function calculateBMI(weight, weightUnit, height, heightUnit) {
+    // BMI = [weight(kg) / height(cm) / height(cm)] * 10,000
+
+    if (weightUnit === "LBS") {
+        weight *= KILOGRAMS_PER_POUND;
+    }
+
+    if (heightUnit === "IN") {
+        height *= CENTIMETERS_PER_INCH;
+    }
+
+    const BMI = ((weight / height) / height) * CM2_PER_M2;
+
+    return BMI.toFixed(1);
+}
+
+
+function printOutput(TDEE, BMI, gender) {
+    safeMinCalories = (gender === "M") ? MIN_CAL_MALE : MIN_CAL_FEMALE;
+
+    BMI = parseFloat(BMI);
+
+    if (BMI < 18.5) {
+        BMI_RANGE = "underweight";
+    }
+    else if (BMI < 25) {
+        BMI_RANGE = "healthy";
+    }
+    else if (BMI < 30) {
+        BMI_RANGE = "overweight";
+    }
+    else {
+        BMI_RANGE = "obese";
+    }
+
+    let infoHTML =
+        `Your TDEE is <strong>${Math.max(TDEE, safeMinCalories)}</strong> calories per day.
+        <br/>
+        Your BMI is <strong>${BMI}</strong>, which is <strong>${BMI_RANGE}</strong>.`;
+
+    let resultsHTML =
+        `To lose 2 lbs/week, eat <strong>${Math.max(TDEE - 1000, safeMinCalories)}</strong> calories per day.<br/>
+        To lose 1 lbs/week, eat <strong>${Math.max(TDEE - 500, safeMinCalories)}</strong> calories per day.<br/>
+        To maintain weight, eat <strong>${Math.max(TDEE, safeMinCalories)}</strong> calories per day.<br/>
+        To gain 1 lbs/week, eat <strong>${Math.max(TDEE + 500, safeMinCalories)}</strong> calories per day.<br/>
+        To lose 2 lbs/week, eat <strong>${Math.max(TDEE + 1000, safeMinCalories)}</strong> calories per day.`;
+
+    const safeMinCaloriesRegex = new RegExp(safeMinCalories, "g");
+    document.querySelector("#resultsContainer").innerHTML = resultsHTML.replace(safeMinCaloriesRegex, `<abbr title='${((gender === "M") ? "Men" : "Women")} are not advised to consume less than ${safeMinCalories} calories per day.'>${safeMinCalories}</abbr>`);
+    document.querySelector("#infoContainer").innerHTML = infoHTML.replace(safeMinCaloriesRegex, `<abbr title='${((gender === "M") ? "Men" : "Women")} are not advised to consume less than ${safeMinCalories} calories per day.'>${safeMinCalories}</abbr>`);
+
+    document.querySelector("#resultsContainer").style.visibility = "visible";
+    document.querySelector("#infoContainer").style.visibility = "visible";
+}
+
+
+function formSubmit() {
+    const inputs = {
+        age: -1,
+
+        weight: -1,
+        weightUnit: "LBS",
+
+        height: -1,
+        heightUnit: "IN",
+
+        bodyFatEntered: false,
+        bodyFatPercent: -1,
+
+        gender: "M",
+        activityLevel: -1,
+    };
+
+
+    if (!validateFormInputs(inputs)) {
+        return;
+    }
+    else {
+        const TDEE = (inputs.bodyFatEntered) ? calculateTDEEwithBF(inputs.gender, inputs.weight, inputs.weightUnit, inputs.bodyFatPercent, inputs.activityLevel) : calculateTDEEnoBF(inputs.gender, inputs.age, inputs.weight, inputs.weightUnit, inputs.height, inputs.heightUnit, inputs.activityLevel);
+
+        const BMI = calculateBMI(inputs.weight, inputs.weightUnit, inputs.height, inputs.heightUnit);
+
+        printOutput(TDEE, BMI, inputs.gender);
+    }
+}
+
+
+document.querySelector("#submitBtn").addEventListener("click", formSubmit);
+
+
+
+
--- resources/views/client/home.blade.php
+++ resources/views/client/home.blade.php
@@ -28,18 +28,20 @@
                 <div class="portfolio-image">
                     <img alt="image" style="margin-top: -10px; pointer-events: auto;" onContextMenu="return false;"
                         width="100%" src="{{ asset('images/passionfood/banner/bannermobile2.png') }}">
-                        <div class="container-fluid" style="padding:0; position:absolute; margin:-60% 2% 20% 2%;">
-                            <div class="row" style="">
-                                <div class="col-12">
-                                    <h1 style="text-align:center; color:#fff; font-size:2.5rem; font-family:'HussarBold' !important; margin-bottom:0px !important;" data-animate="fadeInUp" >
-                                        I’M NOT DIETING<br>
-                                        I’M CHANGING</h1>
-                                    <h1 class="mt-1"
-                                        style="text-align:center; color:#fff; font-size:2.0rem; font-family:'Rondal-Regular' !important; margin-bottom:0px !important;" data-animate="fadeInUp">
-                                        LIFESTYLE</h1>
-                                </div>
+                    <div class="container-fluid" style="padding:0; position:absolute; margin:-60% 2% 20% 2%;">
+                        <div class="row" style="">
+                            <div class="col-12">
+                                <h1 style="text-align:center; color:#fff; font-size:2.5rem; font-family:'HussarBold' !important; margin-bottom:0px !important;"
+                                    data-animate="fadeInUp">
+                                    I’M NOT DIETING<br>
+                                    I’M CHANGING</h1>
+                                <h1 class="mt-1"
+                                    style="text-align:center; color:#fff; font-size:2.0rem; font-family:'Rondal-Regular' !important; margin-bottom:0px !important;"
+                                    data-animate="fadeInUp">
+                                    LIFESTYLE</h1>
                             </div>
                         </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -49,8 +51,9 @@
     <section class="d-none d-lg-block d-xl-block" style="padding:10;">
         <div class="container">
             <div class="row">
-                <div class="col-lg-6 col-xs-12" data-animate="fadeInLeft"><img src="{{ asset('images/passionfood/aboutus/food1.jpg') }}"
-                        style="display:block; margin:auto;" onContextMenu="return false;" width="90%"></div>
+                <div class="col-lg-6 col-xs-12" data-animate="fadeInLeft"><img
+                        src="{{ asset('images/passionfood/aboutus/food1.jpg') }}" style="display:block; margin:auto;"
+                        onContextMenu="return false;" width="90%"></div>
                 <div class="col-lg-6 col-xs-12" style="padding:0px;" data-animate="fadeInRight">
                     <h1 class="text-center mt-5" style="font-size:2.4rem; color:#42623D;">เกี่ยวกับเรา</h1>
                     <p class="aboutusfont">อาหารคลีนที่มีคอนเซ็ป “สุขภาพดีเเบบยั่งยืน”<br>
@@ -72,7 +75,8 @@
         <div class="container">
             <div class="row">
                 <div class="col-xs-12">
-                    <h1 class="text-center mt-5" data-animate="fadeInUp" style="font-size:2.4rem; color:#42623D; margin-bottom:0px;">เกี่ยวกับเรา</h1>
+                    <h1 class="text-center mt-5" data-animate="fadeInUp"
+                        style="font-size:2.4rem; color:#42623D; margin-bottom:0px;">เกี่ยวกับเรา</h1>
                     <p class="aboutusfont" data-animate="fadeInUp">อาหารคลีนที่มีคอนเซ็ป “สุขภาพดีเเบบยั่งยืน”<br>
                         เเละ “I’m not dieting but i’m changing life style”
                         ธุรกิจนี้เกิดจากความรักในสุขภาพเเละการทำอาหารเป็นทุนเดิม
@@ -94,18 +98,20 @@
     {{-- ทำไมต้องเลือกทานอาหารคลีน --}}
     <section style="padding:0;">
         <div class="text-center mt-5">
-            <span class="whyeatfont" data-animate="fadeInUp">ทำไมต้องเลือกทานอาหารคลีนกับ <span class="whyeatfont2">PASSIONFOOD</span></span>
+            <span class="whyeatfont" data-animate="fadeInUp">ทำไมต้องเลือกทานอาหารคลีนกับ <span
+                    class="whyeatfont2">PASSIONFOOD</span></span>
         </div>
-        <img class="mt-5" src="{{ asset('images/passionfood/img/img35.png') }}" data-animate="fadeInLeft" onContextMenu="return false;"
-            width="100%">
+        <img class="mt-5" src="{{ asset('images/passionfood/img/img35.png') }}" data-animate="fadeInLeft"
+            onContextMenu="return false;" width="100%">
     </section>
 
     {{-- avocado --}}
-    <img class="bgimg2" src="{{ asset('images/passionfood/img/img18.png') }}" onContextMenu="return false;" width="100%">
+    <img class="bgimg2" src="{{ asset('images/passionfood/img/img18.png') }}" onContextMenu="return false;"
+        width="100%">
 
     {{-- How to order food --}}
     <section style="padding:0;">
-        <div class="container"  data-animate="fadeInUp">
+        <div class="container" data-animate="fadeInUp">
             <img class="itemimg2" src="{{ asset('images/passionfood/img/img2.png') }}" onContextMenu="return false;"
                 width="100%">
         </div>
@@ -113,7 +119,7 @@
 
     {{-- Passionfood --}}
     <section style="padding: 0;">
-        <img class="itemimg3"  src="{{ asset('images/passionfood/img/img10.png') }}" onContextMenu="return false;"
+        <img class="itemimg3" src="{{ asset('images/passionfood/img/img10.png') }}" onContextMenu="return false;"
             width="100%">
     </section>
 
@@ -215,13 +221,167 @@
 
     {{-- BMR Calculator --}}
     <section class="d-none d-lg-block d-xl-block"
-        style="padding-top: 0px !important; padding-bottom: 200px; z-index:15; background-color:transparent !important;" >
-        <div class="container" data-animate="fadeInUp">
+        style="padding-top: 0px !important; padding-bottom: 200px; z-index:15; background-color:transparent !important;">
+        <div class="container">
             <h2 class="bmr">BMR CALCULATOR</h2>
             <p class="bmrbody">เครื่องคำนวณหาค่า BMR เเละ TDEE
                 เครื่องคำนวณช่วยวางแผนการลดน้ำหนัก เพื่อให้เพื่อนๆได้คำนวณค่าพลังงานที่เหมาะสมและเพื่อการวางแผน
                 การลดน้ำหนักที่ได้ผล โดยไม่ต้องเสียเวลาคิดเองอีกต่อไปค่ะ</p>
-            <form class="">
+            <div class="card card-body" style="padding: 50px;">
+                <form id="form1" class="form-validate">
+                    <div class="row">
+                        <label style="font-size: 1.4rem;">เพศ</label>
+                        <div class="col-sm-3">
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1"
+                                    value="option1" checked>
+                                <label class="form-check-label" for="gridRadios1" style="font-size: 1.4rem;">ชาย</label>
+                            </div>
+                        </div>
+                        <div class="col-sm-3">
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2"
+                                    value="option2">
+                                <label class="form-check-label" for="gridRadios2" style="font-size: 1.4rem;">หญิง</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row mt-4">
+                        <div class="form-group col-md-6">
+                            <label style="font-size: 1.4rem;">น้ำหนักตัว (kg.)</label>
+                            <input type="text" class="form-control" name="username" placeholder="น้ำหนักตัว (kg.)"
+                                required>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label style="font-size: 1.4rem;">ส่วนสูง (cm.)</label>
+                            <input type="email" class="form-control" name="email" placeholder="ส่วนสูง (cm.)"
+                                required>
+                        </div>
+                    </div>
+                    <div class="row mt-3">
+                        <div class="form-group col-md-6">
+                            <label style="font-size: 1.4rem;">อายุ (ปี)</label>
+                            <input type="email" class="form-control" name="email" placeholder="อายุ (ปี)" required>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label style="font-size: 1.4rem;">ระดับกิจกรรมในเเต่ละวัน</label>
+                            <select class="form-select" name="gender" required>
+                                <option value="">เลือกระดับกิจกรรมในเเต่ละวัน</option>
+                                <option>ทำงานแบบนั่งอยู่กับที่</option>
+                                <option>ออกกำลังกาย หรือเล่นกีฬา แบบเบาๆ 1-3 วันต่อสัปดาห์</option>
+                                <option>ออกกำลังกาย หรือเล่นกัฬา ความหนักปานกลาง 3-5 วันต่อสัปดาห์</option>
+                                <option>ออกกำลังกาย หรือเล่นกีฬา หนัก 6-7 วันต่อสัปดาห์</option>
+                                <option>ออกกำลังกาย หรือเล่นกัฬา หนัก แบบการซ้อมเพื่อการแข่งขัน เป็นประจำทุกวัน</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="row mt-3">
+                        <div class="form-group col-md-6">
+                            <label style="font-size: 1.4rem;">เป้าหมายน้ำหนักที่ต้องการ (kg.)</label>
+                            <input type="text" class="form-control" name="username"
+                                placeholder="เป้าหมายน้ำหนักที่ต้องการ (kg.)" required>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label style="font-size: 1.4rem;">จำนวนวัน</label>
+                            <input type="email" class="form-control" name="email" placeholder="จำนวนวัน" required>
+                        </div>
+                    </div>
+                    <a class="button-73 mt-4" href="#"
+                        style="display:block; margin:auto; border-radius:100px; width:20%;"><span
+                            style="font-size:1.6rem;">กดคำนวณ&nbsp;&nbsp;<i class="fa fa-arrow-down"
+                                style="font-size:1.4rem;" aria-hidden="true"></i></span></a>
+
+                    <div class="card card-body mt-4" style="background-color: #f5f5f5;">
+                        <h3 class="text-center">ปริมาณพลังงานที่ต้องลดต่อวัน (kcal)</h3><br>
+                        <h2 class="text-center" style="font-weight: bold; color:slategrey;">0</h2>
+                    </div>
+                    <div class="card card-body mt-4" style="background-color: #f5f5f5;">
+                        <h3 class="text-center">ปริมาณพลังงานที่แนะนำต่อวัน (kcal)</h3><br>
+                        <h2 class="text-center" style="font-weight: bold; color:slategrey;">0</h2>
+                    </div>
+                </form>
+            </div>
+
+            {{-- <form>
+                <div class="row">
+                    <div class="col-1"></div>
+                    <div class="col-2">
+                        <label class="text-left" style="font-size: 1.4rem;">เลือกเพศ:</label>
+                    </div>
+                    <div class="col-3">
+                        <div class="form-check">
+                            <input class="form-check-input" style="background-color:#DBDBDB; border-color:#DBDBDB;"
+                                name="exampleRadios" id="exampleRadios1" value="option1" checked="" type="radio">
+                            <label class="" style="text-align:left; font-size: 1.4rem;">
+                                เพศหญิง
+                            </label>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div class="form-check">
+                            <input class="form-check-input" style="background-color:#DBDBDB; border-color:#DBDBDB;"
+                                name="exampleRadios" id="exampleRadios2" value="option2" type="radio">
+                            <label class="" style="text-align:left; font-size: 1.4rem;">
+                                เพศชาย
+                            </label>
+                        </div>
+                    </div>
+                    <div class="col-3"></div>
+                </div>
+                <div class="row">
+                    <div class="col-1"></div>
+                    <div class="col-2">
+                        <label class="text-left" style="font-size: 1.4rem;"></label>
+                    </div>
+                    <div class="col-3">
+                        <div class="row">
+                            <div class="col-6">
+                                <label class="" style="text-align:right; font-size: 1.4rem;">&nbsp;ส่วนสูง
+                                    (ซม.)&nbsp;</label>
+                            </div>
+                            <div class="col-6">
+                                <input style="width: 100% !important; background-color:#DBDBDB;" type="email"
+                                    class="form-control" id="inputEmail4">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div class="row">
+                            <div class="col-6">
+                                <label class="" style="text-align:right; font-size: 1.4rem;">&nbsp;ส่วนสูง
+                                    (ซม.)&nbsp;</label>
+                            </div>
+                            <div class="col-6">
+                                <input style="width: 100% !important; background-color:#DBDBDB;" type="email"
+                                    class="form-control" id="inputEmail4">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-3"></div>
+                </div>
+                <div class="row">
+                    <div class="col-1"></div>
+                    <div class="col-2">
+                        <label class="text-left" style="font-size: 1.4rem;"></label>
+                    </div>
+                    <div class="col-3">
+                        <div class="row">
+                            <div class="col-6">
+                                <label class="" style="text-align:right; font-size: 1.4rem;">&nbsp;น้ำหนัก
+                                    (กก.)&nbsp;</label>
+                            </div>
+                            <div class="col-6">
+                                <input style="width: 100% !important; background-color:#DBDBDB;" type="email"
+                                    class="form-control" id="inputEmail4">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-3"></div>
+                    <div class="col-3"></div>
+                </div>
+            </form> --}}
+
+            {{-- <form class="">
                 <div class="row mt-4">
                     <div class="col-1" style="padding: 0;"></div>
                     <div class="col-2" style="padding: 0;">
@@ -281,17 +441,35 @@
                             <option>3</option>
                             <option>4</option>
                             <option>5</option>
-                        </select></div>
+                        </select>
+                    </div>
+                    <div class="col-6"></div>
+                    <div class="col-1" style="padding: 0;"></div>
+                    <div class="col-3" style="padding: 0;">
+                        <label style="text-align:left; font-size: 1.2rem; ">เป้าหมายน้ำหนักที่ต้องการ (kg.)</label>
+                    </div>
+                    <div class="col-2" style="padding: 0;">
+                        <input style="width: 100% !important; background-color:#DBDBDB;" type="email"
+                            class="form-control" id="inputEmail4">
+                    </div>
+                    <div class="col-1" style="padding: 0;">
+                        <label style="text-align:center; font-size: 1.2rem; ">จำนวนวัน</label>
+                    </div>
+                    <div class="col-2" style="padding: 0;">
+                        <input style="width: 100% !important; background-color:#DBDBDB;" type="email"
+                            class="form-control" id="inputEmail4">
+                    </div>
                 </div>
                 <a class="button-73 mt-4" href="#"
                     style="display:block; margin:auto; border-radius:100px; width:20%;"><span
-                        style="font-size:1.6rem;">กดคำนวณ&nbsp;&nbsp;<i class="fa fa-arrow-right" style="font-size:1.6rem;"
-                            aria-hidden="true"></i></span></a>
-            </form>
+                        style="font-size:1.6rem;">กดคำนวณ&nbsp;&nbsp;<i class="fa fa-arrow-right"
+                            style="font-size:1.6rem;" aria-hidden="true"></i></span></a>
+            </form> --}}
         </div>
     </section>
     <section class="d-block d-lg-none"
-        style="padding-top: 0px !important; padding-bottom: 80px; z-index:15; background-color:transparent !important;" data-animate="fadeInUp">
+        style="padding-top: 0px !important; padding-bottom: 80px; z-index:15; background-color:transparent !important;"
+        data-animate="fadeInUp">
         <div class="container">
             <h2 class="bmr">BMR CALCULATOR</h2>
             <p class="bmrbody">เครื่องคำนวณหาค่า BMR เเละ TDEE
